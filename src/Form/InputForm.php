464e4884ccbda30ea7f8b2f576dcf16c05ccb04b
Andrey Zakhvatoshin <mastetandron@gmail.com>
2024-04-02 14:14:11 +0300

    форма ввода данных

---
+++ src/Form/InputForm.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+
+class InputForm extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('firstname', TextType::class)
+            ->add('lastname', TextType::class)
+            ->add('description', TextType::class)
+        ;
+    }
+}
